7a151193f26dd806df07a52b832383d3fb23b697
zzq <2403504212@qq.com>
2025-12-01 18:34:23 +0800

    增加动态效果

--- assets/scripts/buttons/complaintButton.ts
+++ assets/scripts/buttons/complaintButton.ts
@@ -1,14 +1,17 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, director, Node } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('startButton')
 export class startButton extends Component {
     start() {
-
+        this.node.on(Node.EventType.TOUCH_START,()=>{
+            director.loadScene('complaint');
+        })
     }
 
     update(deltaTime: number) {
         
     }
 }
-
+
+
--- assets/scripts/buttons/praiseButton.ts
+++ assets/scripts/buttons/praiseButton.ts
@@ -1,14 +1,18 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, director, Node } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('startButton')
 export class startButton extends Component {
     start() {
-
+        this.node.on(Node.EventType.TOUCH_START,()=>{
+            director.loadScene('praise');
+        }
+        )
     }
 
     update(deltaTime: number) {
         
     }
 }
-
+
+
